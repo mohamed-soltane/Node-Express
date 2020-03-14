52b3906da2b1c81fdab6c339500e2ef5f5befabc
Soltane <soltanne92@gmail.com>
2020-03-14 17:40:41 -0500

    partnesRouter and promotionRouter

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,6 +14,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/campsites', campsiteRouter);
+app.use('/promotion', promotionRouter);
+app.use('/partner', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
